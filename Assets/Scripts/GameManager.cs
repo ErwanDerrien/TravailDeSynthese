99a4578264f55d164ffb6131bea4664002d4e9a8
Erwan Derrien <wan@urzhiataer.lan>
2022-02-10 19:57:01 -0500

    fixed unresponsive UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
+using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
 {
+
+    [SerializeField] Button middle;
+    [SerializeField] TMP_Text text;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +24,7 @@ public class GameManager : MonoBehaviour
         
     }
 
-    public void SelectQuestionQuantity(int quantity) {
-        Console.write("Quantity : " + quantity);
+    public void ___SelectQuestionQuantity(int quantity) {
+        Debug.Log("Quantity : " + quantity);
     }
 }
