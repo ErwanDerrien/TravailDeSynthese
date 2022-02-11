474e71f9d731f3fd66f79fe4c020588ef71746c2
Erwan Derrien <wan@urzhiataer.lan>
2022-02-10 21:31:43 -0500

    finished question quantity select

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,17 +5,21 @@ using TMPro;
 
 using UnityEngine.UI;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
 
-    [SerializeField] Button middle;
-    [SerializeField] TMP_Text text;
+    [SerializeField] Button Button5;
+    [SerializeField] Button Button10;
+    [SerializeField] Button Button15;
+
+    int questionQuantity;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Quantity : " + questionQuantity);
     }
 
     // Update is called once per frame
@@ -24,7 +28,10 @@ public class GameManager : MonoBehaviour
         
     }
 
-    public void ___SelectQuestionQuantity(int quantity) {
+    public void SelectQuestionQuantity(int quantity) {
         Debug.Log("Quantity : " + quantity);
+        questionQuantity = quantity;
+        SceneManager.LoadScene("QuestionsPage");
     }
+
 }
